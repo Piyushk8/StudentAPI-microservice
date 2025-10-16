6333704f949f199fab333534bbb58bb7328046e4
Piyush kumar <piyush0808k@gmail.com>
2025-10-17 01:38:48 +0530

    requests validation

--- cmd/studentAPI/main.go
+++ cmd/studentAPI/main.go
@@ -1,33 +1,64 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
+	"log/slog"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/piyushk8/StudentAPI/internal/config"
+	student "github.com/piyushk8/StudentAPI/internal/http/handlers"
 )
 
+func test() {
+	messageChain := make(chan string)
+
+	messageChain <- "ping"
+
+	msg := <-messageChain
+
+	fmt.Println(msg)
+}
+
 func main() {
-	// loaidng configs
+	// test()
+	// // loaidng configs
 	cfg := config.MUSTLoad()
 
 	router := http.NewServeMux()
 
-	router.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
-		w.Write(([]byte("welcome to student api")))
-	})
-	fmt.Print("ddd",cfg.HTTPServer,cfg.Addr)
+	router.HandleFunc("GET /api/student", student.New())
+
 	server := http.Server{
 		Addr:    cfg.Addr,
 		Handler: router,
 	}
 
-	err := server.ListenAndServe()
-	
-	if err != nil {
-		log.Fatal("failed to start server")
-	}
+	go func() {
+		if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatal("failed to start server:", err)
+		}
+	}()
 
-	fmt.Println(("server started"))
-}	
+	slog.Info("server started", slog.String("addr", cfg.Addr))
+
+	done := make(chan os.Signal, 1)
+	signal.Notify(done, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+
+	<-done // wait for Ctrl+C
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	slog.Info("shutting down server")
+	if err := server.Shutdown(ctx); err != nil {
+		slog.Error("server shutdown failed", slog.String("error", err.Error()))
+	} else {
+		slog.Info("server exited gracefully")
+	}
+}
